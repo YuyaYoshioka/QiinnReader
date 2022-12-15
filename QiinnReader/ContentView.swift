40b67b50c921cf33de3cefc07fdc5cc8064115bd
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2022-12-15 22:23:52 +0900

    Initial Commit

---
+++ QiinnReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QiinnReader
+//
+//  Created by 吉岡雄也 on 2022/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QiinnReader/QiinnReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiinnReaderApp.swift
+//  QiinnReader
+//
+//  Created by 吉岡雄也 on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct QiinnReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
